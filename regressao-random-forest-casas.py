b7bd8c839e5212eea0df708282fde2239f0c8064
Israelfluz <israelfaria.gtr@gmail.com>
2019-06-14 09:44:15 -0300

    Regressão com Random Forest na base de dados valor das casas

---
+++ regressao-random-forest-casas.py
@@ -0,0 +1,37 @@
+# ====== importação da biblioteca =======
+import pandas as pd
+
+# ===== carregando a base de dados a ser trabalhanda ======
+base = pd.read_csv('house-prices.csv')
+
+# ===== Variável X que são os atributos previssões ou independentes =====
+X = base.iloc[:, 3:19].values
+
+# ===== Variável Y que são os valores reais das casas onde vamos fazer a previsão =====
+y = base.iloc[:, 2].values
+
+# ==== Criação da base de dadtos trienamento (70%) e base de teste (30%) =======
+from sklearn.model_selection import train_test_split
+X_treinamento, X_teste, y_treinamento, y_teste = train_test_split(X, y,
+                                                                  test_size = 0.3,
+                                                                  random_state = 0)
+
+# ===== Modelo de regressão com Random Forest =====
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 100)
+
+# ===== Gerando o treinamento =====
+regressor.fit(X_treinamento, y_treinamento)
+
+# ==== Visualizando o valor do score para saber como esta se comportando o previssor ====
+score = regressor.score(X_treinamento, y_treinamento)
+
+# ===== Realizando algumas previssões =====
+previsoes = regressor.predict(X_teste)
+
+# ===== Outra maneira de visualizar a diferença =====
+from sklearn.metrics import mean_absolute_error
+mae = mean_absolute_error(y_teste, previsoes)
+
+regressor.score(X_teste, y_teste)
+
